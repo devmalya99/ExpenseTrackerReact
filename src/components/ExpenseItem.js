4272c15a0ae24b534980180682d4d4083528dc87
devmalya99 <debmalyamazumdar99@gmail.com>
2024-01-12 00:52:23 +0530

    Added javascript instead of hard coded info

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+    const expenseDate = new Date(2023,1,1);
+    const expenseTitle='Food';
+    const expenseAmount='545';
+    const LocationOfExpenditure='Kolkata'
   return (
     <div className="expense-item">
-      <div>March 28th 2023</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item_description">
-        <h3>Food</h3>
-        <div className="expense-item_price">100</div>
+        <h3>{expenseTitle}</h3>
+        <div className="expense-item_price">${expenseAmount}</div>
+      <div className="expense-item_location">{LocationOfExpenditure}</div>
       </div>
     </div>
   );
